d3c07599ecb7e8f399e2082e694ec76b25440bd5
jihwooon <jihwooon@gmail.com>
2022-04-04 23:08:00 +0900

    feat : Add NamedJpaQuery

--- src/main/java/study/datajpa/repository/MemberRepository.java
+++ src/main/java/study/datajpa/repository/MemberRepository.java
@@ -1,6 +1,7 @@
 package study.datajpa.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import study.datajpa.entity.Member;
 
 import java.util.List;
@@ -11,4 +12,5 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
 
     List<Member> findTop3HelloBy();
 
+    List<Member> findByUsername(@Param("username") String username);
 }
